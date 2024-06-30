1135bc1f66810f6b4f6ba2a67be66a67f23c03af
Buzimu <54161507+Buzimu@users.noreply.github.com>
2024-06-30 11:47:28 -0500

    Update and rename crow.py to main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+# main.py
+import discord
+from discord.ext import commands
+from config import DISCORD_TOKEN
+from bungie_client import setup_bungie_client
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    await setup_bungie_client()
+    
+    # Load cogs
+    await bot.load_extension('cogs.registration')
+    await bot.load_extension('cogs.titles')
+    await bot.load_extension('cogs.clan_activities')
+
+bot.run(DISCORD_TOKEN)
+
+############################OLD###############################
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
